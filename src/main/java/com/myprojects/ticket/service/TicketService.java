9332f67eb976d79b9762bdd91ea91d2d93fd8cae
ManishGhole <manish.ghole@gmail.com>
2017-08-22 18:08:54 -0400

    Exceptions updated

--- src/main/java/com/myprojects/ticket/service/TicketService.java
+++ src/main/java/com/myprojects/ticket/service/TicketService.java
@@ -2,6 +2,11 @@ package com.myprojects.ticket.service;
 
 import com.myprojects.ticket.service.beans.SeatHold;
 import com.myprojects.ticket.service.exception.TicketServiceException;
+import com.myprojects.ticket.service.exception.InvalidSeatHoldException;
+import com.myprojects.ticket.service.exception.ReservationTimeoutException;
+import com.myprojects.ticket.service.exception.SeatHoldAlreadyConfirmedException;
+import com.myprojects.ticket.service.exception.SeatHoldExpiredException;
+import com.myprojects.ticket.service.exception.SeatHoldTimeoutException;
 import com.myprojects.ticket.service.exception.SeatsUnavailableException;
 
 public interface TicketService {
@@ -9,6 +14,7 @@ public interface TicketService {
 	 * The number of seats in the venue that are neither held nor reserved
 	 *
 	 * @return the number of tickets available in the venue
+	 * @throws TicketServiceException
 	 */
 	int numSeatsAvailable() throws TicketServiceException;
 
@@ -18,17 +24,25 @@ public interface TicketService {
 	 * @param numSeats the number of seats to find and hold
 	 * @param customerEmail unique identifier for the customer
 	 * @return a SeatHold object identifying the specific seats and related information
-	 * @throws SeatsUnavailableException 
-	 * @throws TicketsServiceException
+	 * @throws SeatsUnavailableException
+	 * @throws SeatHoldTimeoutException
+	 * @throws TicketServiceException
 	 */
-	SeatHold findAndHoldSeats(int numSeats, String customerEmail) throws TicketServiceException, SeatsUnavailableException;
+	SeatHold findAndHoldSeats(int numSeats, String customerEmail)
+			throws SeatsUnavailableException, SeatHoldTimeoutException, TicketServiceException;
 
 	/**
 	 * Commit seats held for a specific customer
-	 *
 	 * @param seatHoldId the seat hold identifier
 	 * @param customerEmail the email address of the customer to which the seat hold is assigned
-	 * @return a service confirmation code
+	 * @return
+	 * @throws InvalidSeatHoldException
+	 * @throws SeatHoldExpiredException
+	 * @throws SeatHoldAlreadyConfirmedException
+	 * @throws ReservationTimeoutException
+	 * @throws TicketServiceException
 	 */
-	String reserveSeats(int seatHoldId, String customerEmail) throws TicketServiceException, SeatsUnavailableException;
+	String reserveSeats(int seatHoldId, String customerEmail)
+			throws InvalidSeatHoldException, SeatHoldExpiredException, SeatHoldAlreadyConfirmedException,
+			ReservationTimeoutException, TicketServiceException;
 }
